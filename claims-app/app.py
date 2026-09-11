3e03e8ab249317308af34ee271f860496d2a5ba6
saikumar0210 <saikumarreddy0210@gmail.com>
2026-09-11 15:21:37 +0530

    modidfied yaml

--- claims-app/app.py
+++ claims-app/app.py
@@ -9,14 +9,12 @@ from datetime import date
 from typing import List, Optional
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from nr_logger import Logger
 
 app = FastAPI(title="Motor Insurance Claim Form")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
